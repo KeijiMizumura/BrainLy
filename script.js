f2af5a16e0b9aee744ef21f3792367ac2a6a4fb4
KeijiMizumura <37465305+KeijiMizumura@users.noreply.github.com>
2019-01-01 16:15:15 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,24 @@
 
 var homepage = document.getElementById("home-page");
 
+function onReady(callback) {
+    var intervalId = window.setInterval(function() {
+      if (document.getElementsByTagName('body')[0] !== undefined) {
+        window.clearInterval(intervalId);
+        callback.call(this);
+      }
+    }, 1000);
+}
+
+function setVisible(selector, visible) {
+    document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+    setVisible('.container', true);
+    setVisible('#loader-div', false);
+  });
+
 // Onload function
 window.onload = function(){
     clearFragment();
